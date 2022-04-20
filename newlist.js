8b320285845cc11869c5a4c160cf88ea4cb91209
Catalina Hermoso <93346821+CataHermoso@users.noreply.github.com>
2022-04-20 19:47:49 -0300

    Add files via upload

---
+++ newlist.js
@@ -0,0 +1,6 @@
+const loader = document.getElementById('loader');
+
+startPage = () => {
+    page.classList.remove("hidden"); //the loader appears until the page starts
+    loader.classList.ass("hidden"); //the loader dissapears
+}
